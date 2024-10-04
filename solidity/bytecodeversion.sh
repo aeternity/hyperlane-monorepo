aeternity/hyperlane-monorepo

commit c5c217f8ea1a65c81d54b9118618b284bd370acc

feat: embed package version in deployed bytecode (#4518)
### Description

Embed NPM package version in all deployed contracts bytecode. Follows
[similar pattern to published CLI
version](https://github.com/hyperlane-xyz/hyperlane-monorepo/blob/3010e36ec9ac981fb0e760841c2e9b6a4dde1691/typescript/cli/src/version.ts#L0-L1).

Should not be conflated with `Mailbox.VERSION` used for message
encoding.

### Drive-by Changes

Unfixes solidity NPM package version from typescript packages

### Backward compatibility

No, only applies to new deployments or upgrades

### Testing

Tested [changeset
glob](https://changesets-docs.vercel.app/fixed-packages#using-glob-expressions)
```ts
console.log(micromatch(['@hyperlane-xyz/sdk', '@hyperlane-xyz/core', '@hyperlane-xyz/cli'], ['@hyperlane-xyz/!(core)|*']));
['@hyperlane-xyz/sdk', '@hyperlane-xyz/cli']
```

Added test that version function is on ABI

solidity/bytecodeversion.sh
@@ -1,0 +1,16 @@
+#!/bin/bash
+
+FILEPATH="contracts/PackageVersioned.sol"
+TEMPFILE=$(mktemp)
+
+# writes all but the last 2 lines to the temp file
+head -n $(($(wc -l < $FILEPATH) - 2)) $FILEPATH > $TEMPFILE
+
+# writes generated last 2 lines to the temp file
+cat <<EOF >> $TEMPFILE
+    string public constant PACKAGE_VERSION = "$npm_package_version";
+}
+EOF
+
+# overwrite the original file with the temp file
+cat $TEMPFILE > $FILEPATH

solidity/contracts/AttributeCheckpointFraud.sol
@@ -5,6 +5,7 @@ import {ECDSA} from "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
 import {Address} from "@openzeppelin/contracts/utils/Address.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
+import {PackageVersioned} from "contracts/PackageVersioned.sol";
 import {TREE_DEPTH} from "./libs/Merkle.sol";
 import {CheckpointLib, Checkpoint} from "./libs/CheckpointLib.sol";
 import {CheckpointFraudProofs} from "./CheckpointFraudProofs.sol";
@@ -26,7 +27,8 @@ struct Attribution {
  * @title AttributeCheckpointFraud
  * @dev The AttributeCheckpointFraud contract is used to attribute fraud to a specific ECDSA checkpoint signer.
  */
-contract AttributeCheckpointFraud is Ownable {
+
+contract AttributeCheckpointFraud is Ownable, PackageVersioned {
     using CheckpointLib for Checkpoint;
     using Address for address;
 

solidity/contracts/CheckpointFraudProofs.sol
@@ -9,12 +9,14 @@ import {MerkleLib, TREE_DEPTH} from "./libs/Merkle.sol";
 import {MerkleTreeHook} from "./hooks/MerkleTreeHook.sol";
 import {IMailbox} from "./interfaces/IMailbox.sol";
 
+import {PackageVersioned} from "./PackageVersioned.sol";
+
 struct StoredIndex {
     uint32 index;
     bool exists;
 }
 
-contract CheckpointFraudProofs {
+contract CheckpointFraudProofs is PackageVersioned {
     using CheckpointLib for Checkpoint;
     using Address for address;
 

solidity/contracts/Mailbox.sol
@@ -10,12 +10,19 @@ import {IInterchainSecurityModule, ISpecifiesInterchainSecurityModule} from "./i
 import {IPostDispatchHook} from "./interfaces/hooks/IPostDispatchHook.sol";
 import {IMessageRecipient} from "./interfaces/IMessageRecipient.sol";
 import {IMailbox} from "./interfaces/IMailbox.sol";
+import {PackageVersioned} from "contracts/PackageVersioned.sol";
 
 // ============ External Imports ============
 import {Address} from "@openzeppelin/contracts/utils/Address.sol";
 import {OwnableUpgradeable} from "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 
-contract Mailbox is IMailbox, Indexed, Versioned, OwnableUpgradeable {
+contract Mailbox is
+    IMailbox,
+    Indexed,
+    Versioned,
+    OwnableUpgradeable,
+    PackageVersioned
+{
     // ============ Libraries ============
 
     using Message for bytes;

solidity/contracts/PackageVersioned.sol
@@ -1,0 +1,11 @@
+// SPDX-License-Identifier: MIT OR Apache-2.0
+pragma solidity >=0.6.11;
+
+/**
+ * @title PackageVersioned
+ * @notice Package version getter for contracts
+ **/
+abstract contract PackageVersioned {
+    // GENERATED CODE - DO NOT EDIT
+    string public constant PACKAGE_VERSION = "5.3.0";
+}

solidity/contracts/avs/ECDSAStakeRegistry.sol
@@ -7,6 +7,8 @@ import {IDelegationManager} from "../interfaces/avs/vendored/IDelegationManager.
 import {ISignatureUtils} from "../interfaces/avs/vendored/ISignatureUtils.sol";
 import {IServiceManager} from "../interfaces/avs/vendored/IServiceManager.sol";
 
+import {PackageVersioned} from "../PackageVersioned.sol";
+
 import {OwnableUpgradeable} from "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 import {CheckpointsUpgradeable} from "@openzeppelin/contracts-upgradeable/utils/CheckpointsUpgradeable.sol";
 import {SignatureCheckerUpgradeable} from "@openzeppelin/contracts-upgradeable/utils/cryptography/SignatureCheckerUpgradeable.sol";
@@ -19,7 +21,8 @@ import {IERC1271Upgradeable} from "@openzeppelin/contracts-upgradeable/interface
 contract ECDSAStakeRegistry is
     IERC1271Upgradeable,
     OwnableUpgradeable,
-    ECDSAStakeRegistryStorage
+    ECDSAStakeRegistryStorage,
+    PackageVersioned
 {
     using SignatureCheckerUpgradeable for address;
     using CheckpointsUpgradeable for CheckpointsUpgradeable.History;

solidity/contracts/avs/HyperlaneServiceManager.sol
@@ -19,8 +19,9 @@ import {IAVSDirectory} from "../interfaces/avs/vendored/IAVSDirectory.sol";
 import {IRemoteChallenger} from "../interfaces/avs/IRemoteChallenger.sol";
 import {ISlasher} from "../interfaces/avs/vendored/ISlasher.sol";
 import {ECDSAServiceManagerBase} from "./ECDSAServiceManagerBase.sol";
+import {PackageVersioned} from "contracts/PackageVersioned.sol";
 
-contract HyperlaneServiceManager is ECDSAServiceManagerBase {
+contract HyperlaneServiceManager is ECDSAServiceManagerBase, PackageVersioned {
     // ============ Libraries ============
 
     using EnumerableMapEnrollment for EnumerableMapEnrollment.AddressToEnrollmentMap;

solidity/contracts/client/MailboxClient.sol
@@ -6,12 +6,13 @@ import {IMailbox} from "../interfaces/IMailbox.sol";
 import {IPostDispatchHook} from "../interfaces/hooks/IPostDispatchHook.sol";
 import {IInterchainSecurityModule} from "../interfaces/IInterchainSecurityModule.sol";
 import {Message} from "../libs/Message.sol";
+import {PackageVersioned} from "../PackageVersioned.sol";
 
 // ============ External Imports ============
 import {Address} from "@openzeppelin/contracts/utils/Address.sol";
 import {OwnableUpgradeable} from "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 
-abstract contract MailboxClient is OwnableUpgradeable {
+abstract contract MailboxClient is OwnableUpgradeable, PackageVersioned {
     using Message for bytes;
 
     IMailbox public immutable mailbox;

commit 2445e617d976c1d07c0a832632a90cb32592db15

fix: monorepo docker build (#4626)
### Description

fix: monorepo docker build, which seems to have been broken from
https://github.com/hyperlane-xyz/hyperlane-monorepo/commit/c5c217f8ea1a65c81d54b9118618b284bd370acc#diff-ec261746751beaecd8c9dfe0ad06d2be8e74889eafbe5f7fcbb025726fb2cd90R67

### Drive-by changes

na

### Related issues

na

### Backward compatibility

y

### Testing

trying

---------

Signed-off-by: pbio <10051819+paulbalaji@users.noreply.github.com>

solidity/bytecodeversion.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 FILEPATH="contracts/PackageVersioned.sol"
 TEMPFILE=$(mktemp)