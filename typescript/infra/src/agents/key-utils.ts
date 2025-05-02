aeternity/hyperlane-monorepo

commit cf826875074188513a88225903212238515cd75a

feat:remove create-key circular dependency (#3159)
### Description

Diagnosis: circular dependency created by igp requiring multisig size
and threshold for building coreConfig which is needed while creating new
keys. This arises in the scenario where you add the chain to
chainMetadata and not to defaultMultisigConfigs (say newChainConfigs)

- separated agents and helloworld configs from core and segregated
scripts for `core-utils.ts` and `agent-utils.ts` for avoiding loading of
envConfig.coreConfig
- add argv commands to prompt for new chains or updated chains and
validator/threshold combo:
`yarn ts-node scripts/create-keys -e testnet4 --context hyperlane
--new-chains basesepolia=2/3` (here 2 is desired threshold and 3 is
desired multisig set size)
- if you provide a newChainConfigs chain and don't supply an argv, the
create-keys script will revert
- persist addresses in gcp (already present) and under sdk (this PR)
with types preserved

### Drive-by changes

- imports
- might break other scripts like delete-keys, etc in infra (not tested)

### Related issues

- fixes https://github.com/hyperlane-xyz/issues/issues/887

### Backward compatibility

Yes

### Testing

Manual

---------

Co-authored-by: Yorke Rhodes <yorke@hyperlane.xyz>

typescript/infra/src/agents/key-utils.ts
@@ -1,23 +1,39 @@
 import debug from 'debug';
+import fs from 'fs';
+import path from 'path';
 
 import { ChainMap, ChainName } from '@hyperlane-xyz/sdk';
-import { objMap } from '@hyperlane-xyz/utils';
+import { Address, objMap } from '@hyperlane-xyz/utils';
 
+import localAWMultisigAddresses from '../../config/aw-multisig.json';
+// AW - Abacus Works
 import { Contexts } from '../../config/contexts';
-import { getHelloWorldConfig } from '../../scripts/helloworld/utils';
-import { getEnvironmentConfig } from '../../scripts/utils';
+import { helloworld } from '../../config/environments/helloworld';
+import localKathyAddresses from '../../config/kathy.json';
+import localRelayerAddresses from '../../config/relayer.json';
+import { getJustHelloWorldConfig } from '../../scripts/helloworld/utils';
 import {
   AgentContextConfig,
   DeployEnvironment,
   RootAgentConfig,
 } from '../config';
 import { Role } from '../roles';
-import { fetchGCPSecret, setGCPSecret } from '../utils/gcloud';
 import { execCmd, isEthereumProtocolChain } from '../utils/utils';
 
 import { AgentAwsKey } from './aws/key';
 import { AgentGCPKey } from './gcp';
 import { CloudAgentKey } from './keys';
+
+export type LocalRoleAddresses = Record<
+  DeployEnvironment,
+  Record<Contexts, Address>
+>;
+export const relayerAddresses: LocalRoleAddresses =
+  localRelayerAddresses as LocalRoleAddresses;
+export const kathyAddresses: LocalRoleAddresses =
+  localKathyAddresses as LocalRoleAddresses;
+export const awMultisigAddresses: ChainMap<{ validators: Address[] }> =
+  localAWMultisigAddresses as ChainMap<{ validators: Address[] }>;
 
 const debugLog = debug('infra:agents:key:utils');
 
@@ -69,9 +85,8 @@ function getRoleKeyMapPerChain(
     const validators = agentConfig.validators;
     for (const chainName of agentConfig.contextChainNames.validator) {
       let chainValidatorKeys = {};
-
       const validatorCount =
-        validators?.chains[chainName].validators.length ?? 0;
+        validators?.chains[chainName]?.validators.length ?? 1;
       for (let index = 0; index < validatorCount; index++) {
         const { validator, chainSigner } = getValidatorKeysForChain(
           agentConfig,
@@ -104,9 +119,8 @@ function getRoleKeyMapPerChain(
   };
 
   const setKathyKeys = () => {
-    const envConfig = getEnvironmentConfig(agentConfig.runEnv);
-    const helloWorldConfig = getHelloWorldConfig(
-      envConfig,
+    const helloWorldConfig = getJustHelloWorldConfig(
+      helloworld[agentConfig.runEnv as 'mainnet3' | 'testnet4'], // test doesn't have hello world configs
       agentConfig.context,
     );
     // Kathy is only needed on chains where the hello world contracts are deployed.
@@ -325,11 +339,8 @@ export async function createAgentKeysIfNotExists(
     }),
   );
 
-  await persistAddresses(
-    agentConfig.runEnv,
-    agentConfig.context,
-    keys.map((key) => key.serializeAsAddress()),
-  );
+  await persistAddressesLocally(agentConfig, keys);
+  return;
 }
 
 export async function deleteAgentKeys(agentConfig: AgentContextConfig) {
@@ -352,52 +363,101 @@ export async function rotateKey(
   debugLog(`Rotating key for ${role} on ${chainName}`);
   const key = getCloudAgentKey(agentConfig, role, chainName);
   await key.update();
-  const keyIdentifier = key.identifier;
-  const addresses = await fetchGCPKeyAddresses(
-    agentConfig.runEnv,
-    agentConfig.context,
-  );
-  const filteredAddresses = addresses.filter((_) => {
-    return _.identifier !== keyIdentifier;
-  });
-
-  filteredAddresses.push(key.serializeAsAddress());
-  await persistAddresses(
-    agentConfig.runEnv,
-    agentConfig.context,
-    filteredAddresses,
-  );
+  await persistAddressesLocally(agentConfig, [key]);
 }
 
-async function persistAddresses(
-  environment: DeployEnvironment,
-  context: Contexts,
-  keys: KeyAsAddress[],
+async function persistAddressesLocally(
+  agentConfig: AgentContextConfig,
+  keys: CloudAgentKey[],
 ) {
   debugLog(
-    `Persisting addresses to GCP for ${context} context in ${environment} environment`,
+    `Persisting addresses to GCP for ${agentConfig.context} context in ${agentConfig.runEnv} environment`,
   );
-  await setGCPSecret(
-    addressesIdentifier(environment, context),
-    JSON.stringify(keys),
-    {
-      environment,
-      context,
-    },
+  // recent keys fetched from aws saved to local artifacts
+  const multisigValidatorKeys: ChainMap<{ validators: Address[] }> = {};
+  let relayer, kathy;
+  for (const key of keys) {
+    if (key.role === Role.Relayer) {
+      if (relayer)
+        throw new Error('More than one Relayer found in gcpCloudAgentKeys');
+      relayer = key.address;
+    }
+    if (key.role === Role.Kathy) {
+      if (kathy)
+        throw new Error('More than one Kathy found in gcpCloudAgentKeys');
+      kathy = key.address;
+    }
+    if (!key.chainName) continue;
+    multisigValidatorKeys[key.chainName] ||= {
+      validators: [],
+    };
+    if (key.chainName)
+      multisigValidatorKeys[key.chainName].validators.push(key.address);
+  }
+  if (!relayer) throw new Error('No Relayer found in awsCloudAgentKeys');
+  if (!kathy) throw new Error('No Kathy found in awsCloudAgentKeys');
+  await persistRoleAddressesToLocalArtifacts(
+    Role.Relayer,
+    agentConfig.runEnv,
+    agentConfig.context,
+    relayer,
+    relayerAddresses,
   );
+  await persistRoleAddressesToLocalArtifacts(
+    Role.Kathy,
+    agentConfig.runEnv,
+    agentConfig.context,
+    kathy,
+    kathyAddresses,
+  );
+  await persistValidatorAddressesToLocalArtifacts(multisigValidatorKeys);
 }
 
-async function fetchGCPKeyAddresses(
+// non-validator roles
+export async function persistRoleAddressesToLocalArtifacts(
+  role: Role,
   environment: DeployEnvironment,
   context: Contexts,
+  updated: Address,
+  addresses: Record<DeployEnvironment, Record<Contexts, Address>>,
 ) {
+  addresses[environment][context] = updated;
+
+  // Resolve the relative path
+  const filePath = path.resolve(__dirname, `../../config/${role}.json`);
+
+  fs.writeFileSync(filePath, JSON.stringify(addresses, null, 2));
+}
+
+// maintaining the multisigIsm schema sans threshold
+export async function persistValidatorAddressesToLocalArtifacts(
+  fetchedValidatorAddresses: ChainMap<{ validators: Address[] }>,
+) {
+  for (const chain of Object.keys(fetchedValidatorAddresses)) {
+    awMultisigAddresses[chain] = {
+      validators: fetchedValidatorAddresses[chain].validators, // fresh from aws
+    };
+  }
+  // Resolve the relative path
+  const filePath = path.resolve(__dirname, '../../config/aw-multisig.json');
+  // Write the updated object back to the file
+  fs.writeFileSync(filePath, JSON.stringify(awMultisigAddresses, null, 2));
+}
+
+export function fetchLocalKeyAddresses(
+  role: Role,
+  environment: DeployEnvironment,
+  context: Contexts,
+): Address {
+  // Resolve the relative path
+  const filePath = path.resolve(__dirname, `../../config/${role}.json`);
+  const data = fs.readFileSync(filePath, 'utf8');
+  const addresses: LocalRoleAddresses = JSON.parse(data);
+
   debugLog(
     `Fetching addresses from GCP for ${context} context in ${environment} environment`,
   );
-  const addresses = await fetchGCPSecret(
-    addressesIdentifier(environment, context),
-  );
-  return addresses as KeyAsAddress[];
+  return addresses[environment][context];
 }
 
 function addressesIdentifier(

typescript/infra/config/environments/agents.ts
@@ -1,0 +1,9 @@
+import { agents as mainnet3Agents } from './mainnet3/agent';
+import { agents as testAgents } from './test/agent';
+import { agents as testnet4Agents } from './testnet4/agent';
+
+export const agents = {
+  mainnet3: mainnet3Agents,
+  testnet4: testnet4Agents,
+  test: testAgents,
+};

typescript/infra/config/environments/helloworld.ts
@@ -1,0 +1,7 @@
+import { helloWorld as mainnet3HelloWorld } from './mainnet3/helloworld';
+import { helloWorld as testnet4HelloWorld } from './testnet4/helloworld';
+
+export const helloworld = {
+  mainnet3: mainnet3HelloWorld,
+  testnet4: testnet4HelloWorld,
+};

typescript/infra/config/environments/mainnet3/index.ts
@@ -3,7 +3,7 @@ import { RpcConsensusType } from '@hyperlane-xyz/sdk';
 import {
   getKeysForRole,
   getMultiProviderForRole,
-} from '../../../scripts/utils';
+} from '../../../scripts/agent-utils';
 import { EnvironmentConfig } from '../../../src/config';
 import { Role } from '../../../src/roles';
 import { Contexts } from '../../contexts';

typescript/infra/config/environments/testnet4/index.ts
@@ -3,7 +3,7 @@ import { RpcConsensusType } from '@hyperlane-xyz/sdk';
 import {
   getKeysForRole,
   getMultiProviderForRole,
-} from '../../../scripts/utils';
+} from '../../../scripts/agent-utils';
 import { EnvironmentConfig } from '../../../src/config';
 import { Role } from '../../../src/roles';
 import { Contexts } from '../../contexts';

typescript/infra/hardhat.config.ts
@@ -12,7 +12,7 @@ import {
 } from '@hyperlane-xyz/sdk';
 import { addressToBytes32 } from '@hyperlane-xyz/utils';
 
-import { Modules, getAddresses } from './scripts/utils';
+import { Modules, getAddresses } from './scripts/agent-utils';
 import { sleep } from './src/utils/utils';
 
 enum MailboxHookType {

typescript/infra/scripts/agent-utils.ts
@@ -11,13 +11,15 @@ import {
   CoreConfig,
   MultiProvider,
   RpcConsensusType,
+  chainMetadata,
   collectValidators,
 } from '@hyperlane-xyz/sdk';
 import { ProtocolType, objMap, promiseObjAll } from '@hyperlane-xyz/utils';
 
 import { Contexts } from '../config/contexts';
-import { environments } from '../config/environments';
+import { agents } from '../config/environments/agents';
 import { ethereumChainNames } from '../config/environments/mainnet3/chains';
+import { validatorBaseConfigsFn } from '../config/environments/utils';
 import { getCurrentKubernetesContext } from '../src/agents';
 import { getCloudAgentKey } from '../src/agents/key-utils';
 import { CloudAgentKey } from '../src/agents/keys';
@@ -124,6 +126,14 @@ export function withKeyRoleAndChain<T>(args: yargs.Argv<T>) {
     .alias('i', 'index');
 }
 
+// missing chains are chains needed which are not as part of defaultMultisigConfigs in sdk/src/consts/ but are in chainMetadata
+export function withMissingChains<T>(args: yargs.Argv<T>) {
+  return args
+    .describe('newChains', 'new chains to add')
+    .string('newChains')
+    .alias('n', 'newChains');
+}
+
 export function assertEnvironment(env: string): DeployEnvironment {
   if (EnvironmentNames.includes(env)) {
     return env as DeployEnvironment;
@@ -133,45 +143,98 @@ export function assertEnvironment(env: string): DeployEnvironment {
   );
 }
 
-export function getEnvironmentConfig(environment: DeployEnvironment) {
-  debugLog(`Getting environment config for ${environment}`);
-  return environments[environment];
-}
-
-export async function getConfigsBasedOnArgs(argv?: {
+// not requiring to build coreConfig to get agentConfig
+export async function getAgentConfigsBasedOnArgs(argv?: {
   environment: DeployEnvironment;
   context: Contexts;
+  newChains: string;
 }) {
-  const { environment, context = Contexts.Hyperlane } = argv
-    ? argv
-    : await withContext(getArgs()).argv;
-  const envConfig = getEnvironmentConfig(environment);
-  const agentConfig = getAgentConfig(context, envConfig);
-  return { envConfig, agentConfig, context, environment };
+  const {
+    environment,
+    context = Contexts.Hyperlane,
+    newChains,
+  } = argv ? argv : await withMissingChains(withContext(getArgs())).argv;
+
+  const newValidatorCounts: ChainMap<number> = {};
+  if (newChains) {
+    const chains = newChains.split(',');
+    for (const chain of chains) {
+      const [chainName, newValidatorCount] = chain.split('=');
+      newValidatorCounts[chainName] = parseInt(newValidatorCount, 10);
+    }
+  }
+
+  const agentConfig = getAgentConfig(context, environment);
+  // check if new chains are needed
+  const missingChains = checkIfValidatorsArePersisted(agentConfig);
+
+  // if you include a chain in chainMetadata but not in the aw-multisig.json, you need to specify the new chain in new-chains
+  for (const chain of missingChains) {
+    if (!Object.keys(newValidatorCounts).includes(chain)) {
+      throw new Error(`Missing chain ${chain} not specified in new-chains`);
+    }
+    const baseConfig = {
+      [Contexts.Hyperlane]: [],
+      [Contexts.ReleaseCandidate]: [],
+      [Contexts.Neutron]: [],
+    };
+    // supplementing with dummy addresses for validator as part of missingChains
+    const validatorsConfig = validatorBaseConfigsFn(environment, context);
+
+    const validators = validatorsConfig(
+      {
+        ...baseConfig,
+        [context]: Array(newValidatorCounts[chain]).fill('0x0'),
+      },
+      chain as Chains,
+    );
+    // the hardcoded fields are not strictly necessary to be accurate for create-keys.ts
+    // ideally would still get them from the chainMetadata
+    if (!agentConfig.validators) {
+      throw new Error('AgentConfig does not have validators');
+    }
+
+    agentConfig.validators.chains[chain] = {
+      interval: chainMetadata[chain].blocks?.estimateBlockTime ?? 1, // dummy value
+      reorgPeriod: chainMetadata[chain].blocks?.reorgPeriod ?? 0, // dummy value
+      validators,
+    };
+  }
+
+  return {
+    agentConfig,
+    context,
+    environment,
+  };
 }
 
 // Gets the agent config of a specific context.
+// without fetching environment config
 export function getAgentConfig(
   context: Contexts,
-  environment: EnvironmentConfig | DeployEnvironment,
+  environment: DeployEnvironment,
 ): RootAgentConfig {
-  const coreConfig =
-    typeof environment == 'string'
-      ? getEnvironmentConfig(environment)
-      : environment;
-
-  debugLog(
-    `Getting agent config for ${context} context in ${coreConfig.environment} environment`,
-  );
-
-  const agentConfig = coreConfig.agents[context];
-  if (!agentConfig)
-    throw Error(
-      `Invalid context ${context} for ${
-        coreConfig.environment
-      } environment, must be one of ${Object.keys(coreConfig.agents)}.`,
+  const agentsForEnvironment = agents[environment] as Record<
+    Contexts,
+    RootAgentConfig
+  >;
+  if (!Object.keys(agents[environment]).includes(context)) {
+    throw new Error(
+      `Context ${context} does not exist in agents for environment ${environment}`,
     );
-  return agentConfig;
+  }
+  return agentsForEnvironment[context];
+}
+
+// check if validators are persisted in agentConfig
+export function checkIfValidatorsArePersisted(
+  agentConfig: RootAgentConfig,
+): Set<ChainName> {
+  const supportedChainNames = agentConfig.contextChainNames.validator;
+  const persistedChainNames = Object.keys(agentConfig.validators?.chains || {});
+  return new Set(
+    supportedChainNames.filter((x) => !persistedChainNames.includes(x)),
+  );
 }
 
 export function getKeyForRole(
@@ -182,8 +245,7 @@ export function getKeyForRole(
   index?: number,
 ): CloudAgentKey {
   debugLog(`Getting key for ${role} role`);
-  const environmentConfig = environments[environment];
-  const agentConfig = getAgentConfig(context, environmentConfig);
+  const agentConfig = getAgentConfig(context, environment);
   return getCloudAgentKey(agentConfig, role, chain, index);
 }
 

typescript/infra/scripts/agents/deploy-agents.ts
@@ -1,6 +1,6 @@
 import { createAgentKeysIfNotExists } from '../../src/agents/key-utils';
 import { HelmCommand } from '../../src/utils/helm';
-import { getConfigsBasedOnArgs } from '../utils';
+import { getConfigsBasedOnArgs } from '../core-utils';
 
 import { AgentCli } from './utils';
 

commit 07fc193893c782a28c6df8b485258112dfda0f2b

fix: persist key addresses in GCP (#6111)
### Description

- Starts persisting key addresses in GCP again, fixing key funder infra
- Regression introduced in
https://github.com/hyperlane-xyz/hyperlane-monorepo/pull/3159/files#diff-604c29d425429647a37bc7b46d72cfddb8f5ba0c40d81609234c36bb2a203b35L372
ages ago, just never realized because we didn't add new keys

### Drive-by changes

<!--
Are there any minor or drive-by changes also included?
-->

### Related issues

<!--
- Fixes #[issue number here]
-->

### Backward compatibility

<!--
Are these changes backward compatible? Are there any infrastructure
implications, e.g. changes that would prohibit deploying older commits
using this infra tooling?

Yes/No
-->

### Testing

<!--
What kind of testing have these changes undergone?

None/Manual/Unit Tests
-->

---------

Co-authored-by: ljankovic-txfusion <lazar@txfusion.io>

typescript/infra/src/agents/key-utils.ts
@@ -1,7 +1,7 @@
 import { join } from 'path';
 
 import { ChainMap, ChainName } from '@hyperlane-xyz/sdk';
-import { Address, objMap, rootLogger } from '@hyperlane-xyz/utils';
+import { Address, deepEquals, objMap, rootLogger } from '@hyperlane-xyz/utils';
 
 import { Contexts } from '../../config/contexts.js';
 import { helloworld } from '../../config/environments/helloworld.js';
@@ -12,6 +12,7 @@ import { getJustHelloWorldConfig } from '../../scripts/helloworld/utils.js';
 import { AgentContextConfig, RootAgentConfig } from '../config/agent/agent.js';
 import { DeployEnvironment } from '../config/environment.js';
 import { Role } from '../roles.js';
+import { fetchGCPSecret, setGCPSecretUsingClient } from '../utils/gcloud.js';
 import {
   execCmd,
   getInfraPath,
@@ -340,6 +341,12 @@ export async function createAgentKeysIfNotExists(
   );
 
   await persistAddressesLocally(agentConfig, keys);
+  // Key funder expects the serialized addresses in GCP
+  await persistAddressesInGcp(
+    agentConfig.runEnv,
+    agentConfig.context,
+    keys.map((key) => key.serializeAsAddress()),
+  );
   return;
 }
 
@@ -364,6 +371,42 @@ export async function rotateKey(
   const key = getCloudAgentKey(agentConfig, role, chainName);
   await key.update();
   await persistAddressesLocally(agentConfig, [key]);
+}
+
+async function persistAddressesInGcp(
+  environment: DeployEnvironment,
+  context: Contexts,
+  keys: KeyAsAddress[],
+) {
+  try {
+    const existingSecret = (await fetchGCPSecret(
+      addressesIdentifier(environment, context),
+      true,
+    )) as KeyAsAddress[];
+    if (deepEquals(keys, existingSecret)) {
+      debugLog(
+        `Addresses already persisted to GCP for ${context} context in ${environment} environment`,
+      );
+      return;
+    }
+  } catch (e) {
+    // If the secret doesn't exist, we'll create it below.
+    debugLog(
+      `No existing secret found for ${context} context in ${environment} environment`,
+    );
+  }
+
+  debugLog(
+    `Persisting addresses to GCP for ${context} context in ${environment} environment`,
+  );
+  await setGCPSecretUsingClient(
+    addressesIdentifier(environment, context),
+    JSON.stringify(keys),
+    {
+      environment,
+      context,
+    },
+  );
 }
 
 async function persistAddressesLocally(

typescript/infra/test/warp-configs.test.ts
@@ -24,8 +24,8 @@ const DEFAULT_TIMEOUT = 100000;
 const warpIdsToSkip = [
   'EZETH/arbitrum-base-blast-bsc-ethereum-fraxtal-linea-mode-optimism-sei-swell-taiko-zircuit',
   'EZETHSTAGE/arbitrum-base-blast-bsc-ethereum-fraxtal-linea-mode-optimism-sei-swell-taiko-zircuit',
-  'USDT/base-celo-fraxtal-ink-lisk-mode-optimism-soneium-superseed-unichain-worldchain-staging',
-  'USDT/base-celo-fraxtal-ink-lisk-mode-optimism-soneium-superseed-unichain-worldchain',
+  'USDT/base-bitlayer-celo-ethereum-fraxtal-ink-linea-lisk-mantle-metal-metis-mode-optimism-ronin-soneium-sonic-superseed-unichain-worldchain',
+  'USDTSTAGING/base-bitlayer-celo-ethereum-fraxtal-ink-linea-lisk-mantle-metal-metis-mode-optimism-ronin-soneium-sonic-superseed-unichain-worldchain',
 ];
 
 async function getConfigsForBranch(branch: string) {